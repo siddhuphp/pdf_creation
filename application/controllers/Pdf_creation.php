389239d5ef7a476591f804be51d42714b4fa5fd2
Siddhu <siddharthaesunuri@gmail.com>
2021-02-25 11:17:27 +0530

    mpdf setup is completed and with sample test

--- application/controllers/Pdf_creation.php
+++ application/controllers/Pdf_creation.php
@@ -1,10 +1,14 @@
 <?php
 defined('BASEPATH') OR exit('No direct script access allowed');
 
+
 class Pdf_creation extends CI_Controller {
 
 	public function index()
 	{
 		echo "Lord ganesh ki jai";
+        $mpdf = new \Mpdf\Mpdf();
+        $mpdf->WriteHTML('<h1>Hello world!</h1>');
+        $mpdf->Output();
 	}
 }
