68d9548a05dbec79bec66eae4e8da4372fa43a23
Siddhu <siddharthaesunuri@gmail.com>
2021-02-25 10:44:49 +0530

    index.php on URL removed and pdf_creation controller added

---
+++ application/controllers/Pdf_creation.php
@@ -0,0 +1,10 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+class Pdf_creation extends CI_Controller {
+
+	public function index()
+	{
+		echo "Lord ganesh ki jai";
+	}
+}
