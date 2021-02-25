5f7f798e46a07a04b3e7959256a82df4b60ee003
Siddhu <siddharthaesunuri@gmail.com>
2021-02-25 13:15:51 +0530

    Added css example, and file save example

--- application/controllers/Pdf_creation.php
+++ application/controllers/Pdf_creation.php
@@ -6,9 +6,48 @@ class Pdf_creation extends CI_Controller {
 
 	public function index()
 	{
-		echo "Lord ganesh ki jai";
-        $mpdf = new \Mpdf\Mpdf();
+		$mpdf = new \Mpdf\Mpdf();
         $mpdf->WriteHTML('<h1>Hello world!</h1>');
         $mpdf->Output();
+	}
+
+    public function sample()
+	{
+		$mpdf = new \Mpdf\Mpdf();
+        $mpdf->WriteHTML('<h1>Hello world!</h1>');
+        $mpdf->Output();
+	}
+
+    public function with_html_page()
+	{
+		$mpdf = new \Mpdf\Mpdf();
+        $html = $this->load->view('html_to_pdf',[],true);
+        $mpdf->WriteHTML($html);
+        $mpdf->Output();
+	}
+
+    public function with_html_page_data()
+	{
+		$mpdf = new \Mpdf\Mpdf();
+        $data["data"] = ["name"=>"Siddhu","age"=>"29"];
+        $html = $this->load->view('html_to_pdf',$data,true);
+        $mpdf->WriteHTML($html);
+        $mpdf->Output();
+	}
+
+    public function save_file()
+	{
+        /* 
+            D => dowonload
+            F => save only
+            $mpdf->Output("FilePath",F);
+        */
+
+		$mpdf = new \Mpdf\Mpdf();
+        $data["data"] = ["name"=>"Siddhu","age"=>"29"];
+        $html = $this->load->view('html_to_pdf',$data,true);
+        $mpdf->WriteHTML($html);
+        $file_path = "assets/pdf/my_pdf.pdf";
+        $mpdf->Output($file_path,"F");
 	}
 }
---
+++ application/views/html_to_pdf.php
@@ -0,0 +1,37 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+ <meta charset="utf-8">
+ <title>Welcome to Codesponsors.com</title>
+</head>
+<body>
+ 
+<div id="container">
+    <h1>Welcome to Codesponsors.com!</h1> 
+    <div id="body">
+        <p>If you are seeing this means, it my work behind it <a href="http://codesponsors.com/">Codesponsors</a>.</p>
+    </div> 
+
+    <h1>Image example</h1> 
+    <div id="body">
+       <img src="<?php echo base_url("assets/images/codesponsor-user.png"); ?>" alt="">
+    </div> 
+
+    <h1>CSS example</h1> 
+    <style media="print">
+        #css { 
+            color: red; 
+        }
+    </style>
+    <div id="body">
+       <p id="css">The appearance of a default mPDF document based on HTML should approximate to its appearance in a browser.</p>      
+    </div> 
+
+    <?php if(isset($data) && !empty($data)){ ?>
+        <h1>Example with dynamic data</h1> 
+        <p> my name is <?php echo $data["name"]; ?> and my age  <?php echo $data["age"]; ?></p>
+    <?php } ?>
+</div>
+ 
+</body>
+</html>
